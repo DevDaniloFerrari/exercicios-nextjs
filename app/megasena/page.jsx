18fcdcc237e2cd1be461eaa117f62068ae23b3fa
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-01-24 06:25:53 -0300

    utilizando do while para validacao

--- app/megasena/page.jsx
+++ app/megasena/page.jsx
@@ -1,16 +1,31 @@
+'use client'
+import NumeroDisplay from "@/components/NumeroDisplay"
 import gerarNumerosAleatorios from "../../functions/numerosAleatorios"
+import { useState } from "react"
 
-export default function megasena(){
-    const numeros = gerarNumerosAleatorios(9)
-
-    console.log(numeros)
+export default function megasena() {
+    const [quantidade, setQuantidade] = useState(1)
+    const [numeros, setNumeros] = useState([])
 
     return (
         <div style={{
             display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
             flexDirection: 'column'
         }}>
-            {numeros.map(numero => <span>{numero}</span>)}
+            <h1>Gerador de Números para Mega Sena</h1>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+            }}>
+                {numeros.map(numero => <NumeroDisplay valor={numero} />)}
+            </div>
+            <div>
+                <input type="number" max={15} min={5} onChange={e => setQuantidade(e.target.value)} value={quantidade}/>
+                <button onClick={() => setNumeros(gerarNumerosAleatorios(quantidade))}>Gerar</button>
+            </div>
         </div>
     )
 }
--- functions/numerosAleatorios.js
+++ functions/numerosAleatorios.js
@@ -1,9 +1,11 @@
-export default function gerarNumerosAleatorios(quantidade){
+export default function gerarNumerosAleatorios(quantidade) {
     let numeros = []
 
-    for (let index = 0; index < quantidade; index++) {
-        numeros.push(Math.round(Math.random() * 100))
+    while (numeros.length < quantidade) {
+        const novoNumero = Math.round(Math.random() * 60)
+        if (!numeros.includes(novoNumero))
+            numeros.push(novoNumero)
     }
 
     return numeros
-}
+} 
